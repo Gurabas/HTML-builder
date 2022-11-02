0a7ec78d35d8a311c4699170fe33f7a98279d662
Gurabas <cuper8555@gmail.com>
2022-11-02 17:54:30 +0300

    feat: add solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const pathToFile = path.join(__dirname, 'text.txt');
+
+const stream = new fs.createReadStream(pathToFile);
+
+stream.pipe(process.stdout);
